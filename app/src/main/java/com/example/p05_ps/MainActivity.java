44841f982c0d4d0032ac72585ad1ace3bb382db4
16022877 <16022877@myrp.edu.sg>
2019-05-16 15:07:45 +0800

    Merge branch 'master' of C:\Users\16022877\AndroidStudioProjects\P05ps with conflicts.

--- app/src/main/java/com/example/p05_ps/MainActivity.java
+++ app/src/main/java/com/example/p05_ps/MainActivity.java
@@ -2,12 +2,57 @@ package com.example.p05_ps;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etSong, etSinger, etYear;
+    RadioGroup rg;
+    RadioButton rb;
+    Button btnInsert, btnShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etSong=findViewById(R.id.editTextTittle);
+        etSinger=findViewById(R.id.editTextSinger);
+        etYear=findViewById(R.id.editTextYear);
+        rg=findViewById(R.id.RadioGroup);
+
+        int selectedButtonId=rg.getCheckedRadioButtonId();
+        rb=findViewById(selectedButtonId);
+
+        btnInsert=findViewById(R.id.buttonInsert);
+        btnShow=findViewById(R.id.buttonShow);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String singer=etSinger.getText().toString();
+                String song=etSong.getText().toString();
+                int year=Integer.parseInt(etYear.getText().toString());
+                int star=Integer.parseInt(rb.getText().toString());
+                DBHelper dbh=new DBHelper(MainActivity.this);
+                long row_affected=dbh.insertSong(song);
+                long row_affected=dbh.insertSinger(singer);
+                long row_affected=dbh.insertYear(year);
+                long row_affected=dbh.insertStar(star);
+                dbh.close();
+
+
+
+
+            }
+        });
+
+
+
+
+
     }
 }
